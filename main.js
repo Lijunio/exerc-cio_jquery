8bafa38e739c47d6273eac9d2444a9371a118f6d
Elias Ribeiro <eliasjunio.riibeiro95@gmail.com>
2024-09-06 11:40:00 -0300

    update

--- main.js
+++ main.js
@@ -1,25 +1,15 @@
 $(document).ready(function() {
-    // Função para adicionar a tarefa
     $('#task-form').on('submit', function(event) {
-        event.preventDefault(); // Evita o recarregamento da página ao submeter o formulário
-
-        // Pega o valor do input
+        event.preventDefault();
         const taskText = $('#task-input').val();
 
-        // Verifica se o input não está vazio
         if (taskText.trim() !== '') {
-            // Cria um novo item <li> com o texto da tarefa
             const newTask = $('<li></li>').text(taskText);
-
-            // Adiciona o item na lista
             $('#task-list').append(newTask);
-
-            // Limpa o campo de input após adicionar a tarefa
             $('#task-input').val('');
         }
     });
-
-    // Evento para riscar a tarefa ao clicar nela
+    
     $('#task-list').on('click', 'li', function() {
         $(this).toggleClass('completed');
     });
